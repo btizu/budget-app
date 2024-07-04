db4d18b14d7f3b40349d86a85bdb478787d387cc
Bianca <btizu@deloittece.com>
2024-07-04 16:41:00 +0300

    Added the routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
 import Dashboard from './components/dashboard/Dashboard';
-
+import Expenses from './components/expenses/Expenses';
+import { Route, Routes } from "react-router-dom";
 function App() {
   return (
-
     <div className="main-container">
-      <Dashboard />
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="expenses" element={<Expenses />} />
+        {/* <Route path="/planning" element={<Planning />} /> */}
+        {/* <Route path="/userinfo/:username" element={<UserInfo />} /> */}
+      </Routes>
     </div>
   );
 }
--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { Tooltip, Typography, IconButton, AppBar, Box } from '@mui/material';
+import { useNavigate, Outlet } from 'react-router-dom'
 
 import CalculateIcon from '@mui/icons-material/Calculate';
 import FunctionsIcon from '@mui/icons-material/Functions';
@@ -8,42 +9,52 @@ import EventNoteIcon from '@mui/icons-material/EventNote';
 import './dashboard.css';
 
 export default function Dashboard() {
+  const navigate = useNavigate();
+
   return (
-    <AppBar position="static" color="primary">
-      <div className='dashboard-container'>
-        <Typography variant="h6" gutterBottom align='left' component="div">
-          <Box fontWeight='fontWeightBold' display='inline'>
-            BUDGET APP
-          </Box>
-        </Typography>
-        <div className='dashboard-menu'>
-          <Tooltip title="Budget view">
-            <IconButton
-              size="small"
-              sx={{ ml: 2 }}
-            >
-              <CalculateIcon sx={{ width: 32, height: 32, color: 'white' }}>B</CalculateIcon>
-            </IconButton>
-          </Tooltip>
+    <>
+      <AppBar position="static" color="primary">
 
-          <Tooltip title="Expenses">
-            <IconButton
-              size="small"
-              sx={{ ml: 2 }}
-            >
-              <FunctionsIcon sx={{ width: 32, height: 32, color: 'white' }}>M</FunctionsIcon>
-            </IconButton>
-          </Tooltip>
+        <div className='dashboard-container'>
+          <Typography variant="h6" gutterBottom align='left' component="div">
+            <Box fontWeight='fontWeightBold' display='inline'>
+              BUDGET APP
+            </Box>
+          </Typography>
 
-          <Tooltip title="Plan budget">
-            <IconButton
-              size="small"
-              sx={{ ml: 2 }}
-            >
-              <EventNoteIcon sx={{ width: 32, height: 32, color: 'white' }}>M</EventNoteIcon>
-            </IconButton>
-          </Tooltip>
+          <div className='dashboard-menu'>
+            <Tooltip title="Budget view">
+              <IconButton
+                size="small"
+                sx={{ ml: 2 }}
+                onClick={() => navigate("/")}
+              >
+                <CalculateIcon sx={{ width: 32, height: 32, color: 'white' }}>B</CalculateIcon>
+              </IconButton>
+            </Tooltip>
+
+            <Tooltip title="Expenses">
+              <IconButton
+                size="small"
+                sx={{ ml: 2 }}
+                onClick={() => navigate("/expenses")}
+              >
+                <FunctionsIcon sx={{ width: 32, height: 32, color: 'white' }}>M</FunctionsIcon>
+              </IconButton>
+            </Tooltip>
+
+            <Tooltip title="Plan budget">
+              <IconButton
+                size="small"
+                sx={{ ml: 2 }}
+              >
+                <EventNoteIcon sx={{ width: 32, height: 32, color: 'white' }}>M</EventNoteIcon>
+              </IconButton>
+            </Tooltip>
+          </div>
         </div>
-      </div> </AppBar>
+      </AppBar >
+      <Outlet />
+    </>
   )
 }
